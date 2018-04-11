591e64fe7c6c6b23450461da45f8667f5134051a
ksHDO <hiddendeathorb@gmail.com>
2018-04-11 09:02:12 -0600

    Add AppBuilder to main solution

---
+++ Factory Ide/AppBuilder/Language.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBuilder
+{
+    abstract class Language
+    {
+        protected string topText = "";
+        protected string bottomText = "";
+        protected string body = "";
+
+        protected ElementTemplate[] languageElements = new ElementTemplate[0];
+
+
+       
+
+        protected void AppendTopText()
+        {
+            body += topText;
+        }
+        protected void AppendBottomText()
+        {
+            body += bottomText;
+        }
+
+        protected ElementTemplate FindTemplate(string name)
+        {
+            var ct = languageElements.Length;
+            for (int j = 0; j < ct; ++j)
+            {
+                if(languageElements[j].name == name)
+                {
+                    return languageElements[j];
+                }
+            }
+            return null;
+        }
+
+
+        protected void ApplyBody(List<ElementInfo> elementList)
+        {
+            var ct = elementList.Count;
+            for (int j = 0; j < ct; j++)
+            {
+                var f = FindTemplate(elementList[j].type);
+                if (f == null)
+                {
+                    //Log
+                    Console.WriteLine("No template found for type");
+                    break;
+                }
+
+                string s = f.Format(elementList[j]);
+                body += s;
+
+            }
+        }
+
+
+        public void BuildApp(string path, List<ElementInfo> elementList)
+        {
+            AppendTopText();
+            ApplyBody(elementList);
+            AppendBottomText();
+
+            Console.WriteLine(body);
+        }
+    }
+}
